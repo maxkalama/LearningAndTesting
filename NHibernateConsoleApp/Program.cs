eb40ee94f0d46dff53ad889c412dfde9cf9cf62a
mk <maxkalama@gmail.com>
2018-06-28 13:00:30 +0300

    nhibernate start

---
+++ NHibernateConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Entity
+    {
+        public string Name { get; set; }
+    }
+}
