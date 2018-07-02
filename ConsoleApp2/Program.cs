1162c4638db88bf0e859e247d955c703250f73b0
mk <maxkalama@gmail.com>
2018-07-02 14:38:16 +0300

    shared library experiment

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using SharedClassLibrary;
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var sharedClassMember = new SharedClass();
+            sharedClassMember.SharedMethod();
+
+            Console.Write("anykey to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SharedClassLibrary/SharedClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SharedClassLibrary
+{
+    public class SharedClass
+    {
+        public void SharedMethod()
+        {
+            Console.WriteLine("Shared Method. Version 4.");
+        }
+    }
+}
