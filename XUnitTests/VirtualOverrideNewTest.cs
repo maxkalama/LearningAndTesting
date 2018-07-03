f1e9ab497ee399bc50e04e03435d988a27180f10
mk <maxkalama@gmail.com>
2018-07-03 17:01:32 +0300

    tests update

--- XUnitTests/VirtualOverrideNewTest.cs
+++ XUnitTests/VirtualOverrideNewTest.cs
@@ -11,23 +11,115 @@ namespace XUnitTests
         NoModifierResult
     }
 
-    public class VirtualOverrideNewAbstractTest
+    public class VirtualOverrideNewAbstractTests
     {
+        class Base
+        {
+            private readonly int i = 10;
+            public virtual int I { get => i; }
 
+            public int j = 20;
+        }
+
+        class Derrived: Base
+        {
+            private readonly int i = 11;
+            public override int I { get => i;  }
+
+            public new int j = 21;
+        }
+
+        [Fact]
+        public void StandaloneTest()
+        {
+            Base derrivedAsBase = new Derrived();
+            Assert.Equal(11, derrivedAsBase.I) ;
+            Assert.Equal(20, derrivedAsBase.j); 
+        }
+
+        #region Tests using base class members
         #region No modifier base class tests
         //[Fact]
-        //public void NoModifierOverrideTest()
+        //public void NoModifierOverrideBaseTest()
+        //{
+        //    NoModifierClass noModifierMember = new NoModifierClass();
+        //    Assert.Equal(ResultEnum.NoModifierResult, noModifierMember.Work());
+
+        //    NoModifierOverrideClass overrideMember = new NoModifierOverrideClass();
+        //    Assert.Equal(ResultEnum.OverrideResult, overrideMember.Work()); //no implementation in derrived class, result is from base class
+        //}
+
+        [Fact]
+        public void NoModifierNewBaseTest()
+        {
+            NoModifierClass noModifierMember = new NoModifierClass();
+            Assert.Equal(ResultEnum.NoModifierResult, noModifierMember.Work());
+
+            NoModifierClass newMember = new NoModifierNewClass();
+            Assert.Equal(ResultEnum.NewResult, newMember.Work());
+        }
+        #endregion
+
+        #region Abstract base class tests
+        [Fact]
+        public void AbstractOverrideBaseTest()
+        {
+            //AbstractClass AbstractMember = new AbstractClass(); //can't create abstract class member
+            //Assert.Equal(ResultEnum.AbstractResult, AbstractMember.Work());
+
+            AbstractClass overrideMember = new AbstractOverrideClass();
+            Assert.Equal(ResultEnum.OverrideResult, overrideMember.Work());
+        }
+
+        //[Fact]
+        //public void AbstractNewBaseTest()
+        //{
+        //    AbstractClass AbstractMember = new AbstractClass(); //can't create abstract class member
+        //    Assert.Equal(ResultEnum.AbstractResult, AbstractMember.Work());
+
+        //    AbstractClass newMember = new AbstractNewClass(); //AbstractNew class is impossible
+        //    Assert.Equal(ResultEnum.NewResult, newMember.Work());
+        //}
+        #endregion
+
+        #region Virtual base class tests
+        [Fact]
+        public void VirtualOverrideBaseTest()
+        {
+            VirtualClass VirtualMember = new VirtualClass();
+            Assert.Equal(ResultEnum.VirtualResult, VirtualMember.Work());
+
+            VirtualClass overrideMember = new VirtualOverrideClass();
+            Assert.Equal(ResultEnum.OverrideResult, overrideMember.Work());
+        }
+
+        [Fact]
+        public void VirtualNewBaseTest()
+        {
+            VirtualClass VirtualMember = new VirtualClass();
+            Assert.Equal(ResultEnum.VirtualResult, VirtualMember.Work());
+
+            VirtualClass newMember = new VirtualNewClass();
+            Assert.Equal(ResultEnum.NewResult, newMember.Work());
+        }
+        #endregion 
+        #endregion
+
+        #region Tests using derrived class members
+        #region No modifier base class tests
+        //[Fact]
+        //public void NoModifierOverrideDerrivedTest()
         //{
         //    NoModifierClass noModifierMember = new NoModifierClass();
         //    Assert.Equal(ResultEnum.NoModifierResult, noModifierMember.Work());
 
         //    NoModifierOverrideClass overrideMember = new NoModifierOverrideClass();
         //    Assert.NotEqual(ResultEnum.OverrideResult, overrideMember.Work());
-        //    Assert.Equal(ResultEnum.NoModifierResult, overrideMember.Work()); // <= no implementation in derrived class, result is from base class
+        //    Assert.Equal(ResultEnum.NoModifierResult, overrideMember.Work()); //no implementation in derrived class, result is from base class
         //}
 
         [Fact]
-        public void NoModifierNewTest()
+        public void NoModifierNewDerrivedTest()
         {
             NoModifierClass noModifierMember = new NoModifierClass();
             Assert.Equal(ResultEnum.NoModifierResult, noModifierMember.Work());
@@ -36,39 +128,120 @@ namespace XUnitTests
             Assert.Equal(ResultEnum.NewResult, newMember.Work());
         }
         #endregion
-        
+
         #region Abstract base class tests
         [Fact]
-        public void AbstractOverrideTest()
+        public void AbstractOverrideDerrivedTest()
         {
-            AbstractClass AbstractMember = new AbstractClass();
-            Assert.Equal(ResultEnum.AbstractResult, AbstractMember.Work());
+            //AbstractClass AbstractMember = new AbstractClass(); //can't create abstract class member
+            //Assert.Equal(ResultEnum.AbstractResult, AbstractMember.Work());
 
             AbstractOverrideClass overrideMember = new AbstractOverrideClass();
-            Assert.NotEqual(ResultEnum.OverrideResult, overrideMember.Work());
-            Assert.Equal(ResultEnum.AbstractResult, overrideMember.Work());
+            Assert.Equal(ResultEnum.OverrideResult, overrideMember.Work());
         }
 
-        [Fact]
-        public void AbstractNewTest()
-        {
-            AbstractClass AbstractMember = new AbstractClass();
-            Assert.Equal(ResultEnum.AbstractResult, AbstractMember.Work());
+        //[Fact]
+        //public void AbstractNewDerrivedTest()
+        //{
+        //    AbstractClass AbstractMember = new AbstractClass(); //can't create abstract class member
+        //    Assert.Equal(ResultEnum.AbstractResult, AbstractMember.Work());
 
-            AbstractNewClass newMember = new AbstractNewClass();
-            Assert.Equal(ResultEnum.NewResult, newMember.Work());
-        }
-#endregion
+        //    AbstractNewClass newMember = new AbstractNewClass(); //AbstractNew class is impossible
+        //    Assert.Equal(ResultEnum.NewResult, newMember.Work());
+        //}
+        #endregion
 
+        #region Virtual base class tests
         [Fact]
-        public void VirtualOverrideTest()
+        public void VirtualOverrideDerrivedTest()
         {
-            VirtualClass virtualMember = new VirtualClass();
-            Assert.Equal(ResultEnum.VirtualResult, virtualMember.Work());
+            VirtualClass VirtualMember = new VirtualClass();
+            Assert.Equal(ResultEnum.VirtualResult, VirtualMember.Work());
 
             VirtualOverrideClass overrideMember = new VirtualOverrideClass();
             Assert.Equal(ResultEnum.OverrideResult, overrideMember.Work());
         }
+
+        [Fact]
+        public void VirtualNewDerrivedTest()
+        {
+            VirtualClass VirtualMember = new VirtualClass();
+            Assert.Equal(ResultEnum.VirtualResult, VirtualMember.Work());
+
+            VirtualNewClass newMember = new VirtualNewClass();
+            Assert.Equal(ResultEnum.NewResult, newMember.Work());
+        }
+        #endregion 
+        #endregion
+
+        #region Tests using interface class members
+        #region No modifier base class tests
+        //[Fact]
+        //public void NoModifierOverrideInterfaceTest()
+        //{
+        //    IWorkable noModifierMember = new NoModifierClass();
+        //    Assert.Equal(ResultEnum.NoModifierResult, noModifierMember.Work());
+
+        //    IWorkable overrideMember = new NoModifierOverrideClass();
+        //    Assert.NotEqual(ResultEnum.OverrideResult, overrideMember.Work());
+        //    Assert.Equal(ResultEnum.NoModifierResult, overrideMember.Work()); //no implementation in derrived class, result is from base class
+        //}
+
+        [Fact]
+        public void NoModifierNewInterfaceTest()
+        {
+            IWorkable noModifierMember = new NoModifierClass();
+            Assert.Equal(ResultEnum.NoModifierResult, noModifierMember.Work());
+
+            IWorkable newMember = new NoModifierNewClass();
+            Assert.Equal(ResultEnum.NewResult, newMember.Work());
+        }
+        #endregion
+
+        #region Abstract base class tests
+        [Fact]
+        public void AbstractOverrideInterfaceTest()
+        {
+            //IWorkable AbstractMember = new AbstractClass(); //can't create abstract class member
+            //Assert.Equal(ResultEnum.AbstractResult, AbstractMember.Work());
+
+            IWorkable overrideMember = new AbstractOverrideClass();
+            Assert.Equal(ResultEnum.OverrideResult, overrideMember.Work());
+        }
+
+        //[Fact]
+        //public void AbstractNewInterfaceTest()
+        //{
+        //    AbstractClass AbstractMember = new AbstractClass(); //can't create abstract class member
+        //    Assert.Equal(ResultEnum.AbstractResult, AbstractMember.Work());
+
+        //    AbstractNewClass newMember = new AbstractNewClass(); //AbstractNew class is impossible
+        //    Assert.Equal(ResultEnum.NewResult, newMember.Work());
+        //}
+        #endregion
+
+        #region Virtual base class tests
+        [Fact]
+        public void VirtualOverrideInterfaceTest()
+        {
+            IWorkable VirtualMember = new VirtualClass();
+            Assert.Equal(ResultEnum.VirtualResult, VirtualMember.Work());
+
+            IWorkable overrideMember = new VirtualOverrideClass();
+            Assert.Equal(ResultEnum.OverrideResult, overrideMember.Work());
+        }
+
+        [Fact]
+        public void VirtualNewInterfaceTest()
+        {
+            IWorkable VirtualMember = new VirtualClass();
+            Assert.Equal(ResultEnum.VirtualResult, VirtualMember.Work());
+
+            IWorkable newMember = new VirtualNewClass();
+            Assert.Equal(ResultEnum.NewResult, newMember.Work());
+        }
+        #endregion 
+        #endregion
     }
 
     interface IWorkable
@@ -119,14 +292,14 @@ namespace XUnitTests
 
     //class AbstractNewClass : AbstractClass, IWorkable
     //{
-    //    //error. asks for new or override. new will fail as below
-    //    //public ResultEnum Work()
+    //    //error. inherited Work() not implemented. not interface related.
+    //    //public new ResultEnum Work()
     //    //{
     //    //    return ResultEnum.NewResult;
     //    //}
-
-    //    //error. inherited Work() not implemented. not interface related.
-    //    //public new ResultEnum Work()
+    //
+    //    //error. asks for new or override. new will fail as above. override is another class.
+    //    //public ResultEnum Work()
     //    //{
     //    //    return ResultEnum.NewResult;
     //    //}
